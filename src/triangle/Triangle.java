1d4e476231e051a70b1fee743556a48a3426b3c7
NzyBiyte <ameli.siedersleben@student.htw-berlin.de>
2025-06-15 19:12:21 +0200

    Lab06 Rcursive Triangles

---
+++ src/triangle/Triangle.java
@@ -0,0 +1,107 @@
+package triangle;
+
+import resizable.ResizableImage;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import static resizable.Debug.print;
+
+/**
+ * Implement your Sierpinski Triangle here.
+ *
+ *
+ * You only need to change the drawTriangle
+ * method!
+ *
+ *
+ * If you want to, you can also adapt the
+ * getResizeImage() method to draw a fast
+ * preview.
+ *
+ */
+public class Triangle implements ResizableImage {
+    int drawTriangle = 0;
+
+    /**
+     * change this method to implement the triangle!
+     *
+     * @param size the outer bounds of the triangle
+     * @return an Image containing the Triangle
+     */
+    private BufferedImage drawTriangle(Dimension size) {
+        print("drawTriangle: " + ++drawTriangle + "size: " + size);
+        BufferedImage bufferedImage = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D gBuffer = (Graphics2D) bufferedImage.getGraphics();
+        gBuffer.setColor(Color.black);
+        int border = 2;
+        gBuffer.drawRect(border, border, size.width - 2 * border, size.height - 2 * border);
+        gBuffer.setColor(Color.darkGray);
+        border = 8;
+        gBuffer.drawRect(border, border, size.width - 2 * border, size.height - 2 * border);
+        gBuffer.drawString("Triangle goes here", border * 2, border * 4);
+
+        // Triangle
+        int[] xPoints = {(size.width -border) / 2, border, size.width -border};
+        int[] yPoints = {border, size.height -border, size.height -border};
+
+        int maxDepth =9;
+
+        // Start recursive
+        drawSierpinski(gBuffer, xPoints, yPoints, maxDepth, maxDepth);
+
+
+        return bufferedImage;
+    }
+
+    /**
+     * Recursively draws the Sierpinski Triangle on the given Graphics2D context.
+     */
+    private void drawSierpinski(Graphics2D g, int[] x, int[] y, int depth, int maxDepth) {
+        //color according fo recursion depth
+        float hue = (float)(maxDepth - depth)/ maxDepth;
+        g.setColor(Color.getHSBColor(hue, 0.6f, 1.0f));
+        g.fillPolygon(x, y, 3);
+        if (depth == 0) {
+            g.setColor(Color.getHSBColor(hue, 1.6f, 1.0f));
+            g.fillPolygon(x, y, 3);
+            return;
+        }
+
+        // Midpoints calculation
+        int x0 = (x[0] + x[1]) / 2;
+        int y0 = (y[0] + y[1]) / 2;
+        int x1 = (x[1] + x[2]) / 2;
+        int y1 = (y[1] + y[2]) / 2;
+        int x2 = (x[2] + x[0]) / 2;
+        int y2 = (y[2] + y[0]) / 2;
+
+        //drawSierpinski(g, new int[]{x[0], x0, x2}, new int[]{y[2], y0, y2}, depth - 1, maxDepth);
+
+        drawSierpinski(g, new int[]{x[0], x0, x2}, new int[]{y[0], y0, y2}, depth - 1, maxDepth);
+        drawSierpinski(g, new int[]{x0, x[1], x1}, new int[]{y0, y[1], y1}, depth - 1, maxDepth);
+        drawSierpinski(g, new int[]{x2, x1, x[2]}, new int[]{y2, y1, y[2]}, depth - 1, maxDepth);
+
+    }
+
+    BufferedImage bufferedImage;
+    Dimension bufferedImageSize;
+
+    @Override
+    public Image getImage(Dimension triangleSize) {
+        if (triangleSize.equals(bufferedImageSize))
+            return bufferedImage;
+        bufferedImage = drawTriangle(triangleSize);
+        bufferedImageSize = triangleSize;
+        return bufferedImage;
+    }
+    @Override
+    public Image getResizeImage(Dimension size) {
+        BufferedImage bufferedImage = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D gBuffer = (Graphics2D) bufferedImage.getGraphics();
+        gBuffer.setColor(Color.pink);
+        int border = 2;
+        gBuffer.drawRect(border, border, size.width - 2 * border, size.height - 2 * border);
+        return bufferedImage;
+    }
+}
